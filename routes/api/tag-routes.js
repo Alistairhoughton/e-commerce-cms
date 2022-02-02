a3c450bbf5214e5be1b372b1e6180eb97e1c8819
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-02-02 17:41:32 +0000

    added in models.

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
+
+
+
+
+// ===========================================================================================
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const driverData = await Driver.findAll({
+//       include: [{ model: License }, { model: Car }],
+//     });
+//     res.status(200).json(driverData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // GET a single driver
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const driverData = await Driver.findByPk(req.params.id, {
+//       include: [{ model: License }, { model: Car }],
+//     });
+
+//     if (!driverData) {
+//       res.status(404).json({ message: 'No driver found with that id!' });
+//       return;
+//     }
+
+//     res.status(200).json(driverData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// module.exports = router;
+
